6f3d51785acd70a6f6b4db678c0fe8b537269123
willi684.purdue.edu <willi684.purdue.edu>
2016-06-29 01:33:09 -0400

    Restart without Skeleton Code
    Actually compiles and runs. Currently displays fake data. Just finished
    the step "Quiz: Finding Views findViewById".

---
+++ Project1/app/src/main/java/com/udacity/caraher/emma/project1/MainActivity.java
@@ -0,0 +1,89 @@
+package com.udacity.caraher.emma.project1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> forecasts = new ArrayList<String>();
+
+            forecasts.add("Today - Sunny - 88/63");
+            forecasts.add("Tomorrow - Foggy - 70/46");
+            forecasts.add("Weds - Cloudy - 72/63");
+            forecasts.add("Thurs - Rainy - 64/51");
+            forecasts.add("Fri - Foggy - 70/46");
+            forecasts.add("Sat - Sunny - 76/68");
+
+            ArrayAdapter<String> newAdapter = new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textView,
+                    forecasts
+            );
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(newAdapter);
+
+            return rootView;
+        }
+    }
+}
