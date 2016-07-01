783f4ffeb2f91e3576d4ebf6d50e3c4bcf360b04
willi684.purdue.edu <willi684.purdue.edu>
2016-07-01 01:07:21 -0400

    Started Detail Activity
    When you click on an item, it now launches a new activity that shows the
    text of the item you just clicked on.

--- Project1/app/src/main/java/com/udacity/caraher/emma/project1/ForecastFragment.java
+++ Project1/app/src/main/java/com/udacity/caraher/emma/project1/ForecastFragment.java
@@ -1,5 +1,7 @@
 package com.udacity.caraher.emma.project1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +14,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -62,7 +66,7 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
@@ -92,6 +96,18 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Object selectedForecast = adapterView.getItemAtPosition(i);
+                String forecastString = selectedForecast.toString();
+
+                Context context = view.getContext();
+                Intent detailIntent = new Intent(context, DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecastString);
+                startActivity(detailIntent);
+            }
+        });
 
         return rootView;
     }
